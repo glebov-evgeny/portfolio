eda98d542f2e9d2a3d441766e2b68fdc6f3e88af
Евгений Глебов <evgeniy@Mac-mini-Evgenij.local>
2021-08-27 22:20:57 +0300

    test

--- .distr/blocks/main/block.php
+++
@@ -1,6 +0,0 @@
-<section class="main" id="main">
-    <div class="container">
-        <img src="img/main/test.jpg" alt="" class="main__pic">
-        <h1 class="main__text">Тест 1</h1>
-    </div>
-</section>
