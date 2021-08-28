8cee57f6e411f68eaead2480a36c5fc7dcad241d
John Glebov <egoji@mail.ru>
2021-08-28 14:47:17 +0300

    test

--- public/index.php
+++ public/index.php
@@ -87,7 +87,7 @@ include_once $ROOT . 'version.php';
                 <div class="about__content-aside_pic"></div>
             </div>
             <div class="about__content-main nonactive">
-                <h2 class="about__content-title">Добрый день!!!</h2>
+                <h2 class="about__content-title">Добрый день</h2>
                 <p class="about__content-text">Меня зовут Глебов Евгений, и&nbsp;я&nbsp;рад приветствовать Вас на&nbsp;страничке моего портфолио.</p>
             </div>
        </div>
